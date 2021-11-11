2f6a8824c7db071349ddfd82bfb2bde65db126fc
mihakim2 <mihakim2@illinois.edu>
2021-11-11 17:51:55 -0600

    Updated the stdin function to find CallSign Meassages in the incoming response

--- Moazam.py
+++ Moazam.py
@@ -4,17 +4,35 @@ Assignment on NetworkX
 Moazam's portion
 """
 import pandas as pd
+from sys import stdin
+import io
 
-def read_csv_flight(path):
-    col_list = ['MessageType', 'TransmissionType', 'SessionID', 'AircraftID', 'AircraftHex', 'FlightRecorderNum',
+total_messages = 0
+callsign_messages = 0
+
+for line in stdin:
+    total_messages += 1
+    data = line.split(",")
+    if len(data[10]) != 0:
+        callsign_messages += 1
+        # print("True")
+    else:
+        pass
+    if total_messages % 500 == 0:
+        print('\nMessage counts: total {} type 3: {}\n'.format(total_messages, callsign_messages))
+
+col_list = ['MessageType', 'TransmissionType', 'SessionID', 'AircraftID', 'AircraftHex', 'FlightRecorderNum',
                 'DateGenerated','TimeGenerated', 'DateLogged', 'TimeLogged', 'Callsign/FlightNum', 'Altitude',
                 'GroundSpeed','Track/Heading','Latitude', 'Longitude', 'VerticalRate', 'SquawkCode',
                 'Alert (Squawk Change)', 'Emergency', 'SPI','OnGround']
-    data = pd.read_csv('sample_log.csv', header=None)
-    data.rename(columns={i: j for i, j in zip(list(data), col_list)}, inplace=True)
-    df = data[~data['Callsign/FlightNum'].isnull()].copy()
-    return df['Callsign/FlightNum']
 
-
-df = read_csv_flight('sample_log.csv')
-print(df.head(100))
+# def read_csv_flight(path):
+#
+#     data = pd.read_csv('sample_log.csv', header=None)
+#     data.rename(columns={i: j for i, j in zip(list(data), col_list)}, inplace=True)
+#     df = data[~data['Callsign/FlightNum'].isnull()].copy()
+#     return df['Callsign/FlightNum']
+#
+#
+# df = read_csv_flight('sample_log.csv')
+# print(df.head(100))
