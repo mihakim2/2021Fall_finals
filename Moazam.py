0ba0ce824f97126ce31bcfc1b3d0a947d4c5fdc7
mihakim2 <mihakim2@illinois.edu>
2021-11-11 18:08:49 -0600

    Updated the stdin function to find CallSign Meassages in the incoming response

--- Moazam.py
+++ Moazam.py
@@ -5,26 +5,27 @@ Moazam's portion
 """
 import pandas as pd
 from sys import stdin
-import io
+import numpy as np
 
 total_messages = 0
 callsign_messages = 0
-
+callsigns= [np.array([],dtype = np.uint32)]
 for line in stdin:
     total_messages += 1
     data = line.split(",")
     if len(data[10]) != 0:
         callsign_messages += 1
-        # print("True")
+        callsigns= np.append(data[10],callsigns)
     else:
         pass
     if total_messages % 500 == 0:
-        print('\nMessage counts: total {} type 3: {}\n'.format(total_messages, callsign_messages))
+        print('\nMessage counts: total {} CallSigns: {}\n'.format(total_messages, callsign_messages))
+print(len(callsigns))
 
-col_list = ['MessageType', 'TransmissionType', 'SessionID', 'AircraftID', 'AircraftHex', 'FlightRecorderNum',
-                'DateGenerated','TimeGenerated', 'DateLogged', 'TimeLogged', 'Callsign/FlightNum', 'Altitude',
-                'GroundSpeed','Track/Heading','Latitude', 'Longitude', 'VerticalRate', 'SquawkCode',
-                'Alert (Squawk Change)', 'Emergency', 'SPI','OnGround']
+# col_list = ['MessageType', 'TransmissionType', 'SessionID', 'AircraftID', 'AircraftHex', 'FlightRecorderNum',
+#                 'DateGenerated','TimeGenerated', 'DateLogged', 'TimeLogged', 'Callsign/FlightNum', 'Altitude',
+#                 'GroundSpeed','Track/Heading','Latitude', 'Longitude', 'VerticalRate', 'SquawkCode',
+#                 'Alert (Squawk Change)', 'Emergency', 'SPI','OnGround']
 
 # def read_csv_flight(path):
 #
